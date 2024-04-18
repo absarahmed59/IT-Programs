adf880c7bf7514edc0844d3f1a1ea6d7237c7fcd
Absar Ahmed <68219741+absarahmed59@users.noreply.github.com>
2024-04-18 22:17:23 +0500

    Create Inheritance03

---
+++ Inheritance03.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Teacher{
+    protected:
+        int age;
+        string name, address;
+    public:
+        void input(){
+            cin>>name>>age>>address;
+        }
+        void print(){
+            cout<<name<<endl<<age<<endl<<address<<endl;
+        }
+};
+class NatPhone: public LocalPhone{
+    protected:
+        int books;
+        string name, address;
+    public:
+        void input(){
+            cin>>name>>books>>address;
+        }
+        void print(){
+            cout<<name<<endl<<books<<endl<<address;
+        }
+};
+class Scholar: public Teacher, public Writer{
+
+};
+
+int main(){
+    
+    return 0;
+}
